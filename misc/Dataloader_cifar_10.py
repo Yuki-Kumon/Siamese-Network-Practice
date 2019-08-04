b7ad9ce5cc538479cc5be7e4df65d8e551ee446d
Yuki Kumon <yuki-kumon@g.ecc.u-tokyo.ac.jp>
2019-08-04 17:20:17 +0900

    データローダーまで。

--- misc/Dataloader_cifar_10.py
+++ misc/Dataloader_cifar_10.py
@@ -10,7 +10,7 @@ Last Update :
 
 
 from torch.utils.data import Dataset
-# from torchvision import transforms
+from torchvision import transforms
 # import torch.nn as nn  # ネットワーク構築用
 # import torch.optim as optim  # 最適化関数
 # import torch.nn.functional as F  # ネットワーク用の様々な関数
@@ -32,16 +32,16 @@ class MyDataset(Dataset):
     dataset class
     '''
 
-    def __init__(self, dict, trans1=None):
+    def __init__(self, dict, trans1=None, same_rate=0.5):
         self.data = dict[b'data']
         self.label = dict[b'labels']
         self.trans1 = trans1
+        self.same_rate = same_rate
 
     def __len__(self):
         return len(self.label)
 
-    def __getitem__(self, idx):
-        data = self.data[idx]
+    def image_reshape(self, data):
         r = np.reshape(data[:1024], (32, 32))
         g = np.reshape(data[1024:2048], (32, 32))
         b = np.reshape(data[2048:], (32, 32))
@@ -51,10 +51,48 @@ class MyDataset(Dataset):
         img[:, :, 2] = b
         img = img.astype(int)
         img = Image.fromarray(np.uint8(img))
+        return img
+
+    def __getitem__(self, idx):
+        data = self.data[idx]
+        img = self.image_reshape(data)
 
         label = self.label[idx]
 
-        if self.trans1:
-            image = self.trans1(img)
+        # same_rateの割合で、同じラベルの画像を取ってくる
+        if np.random.choice([True, False], p=[self.same_rate, 1 - self.same_rate]):
+            same_indices = np.where(np.array(self.label) == label)[0]
+            same_idx = np.random.choice(same_indices, 1)[0]
+            data = self.data[same_idx]
+            img2 = self.image_reshape(data)
+            same_label = 1
+        else:
+            different_indices = np.where(np.array(self.label) != label)[0]
+            different_idx = np.random.choice(different_indices, 1)[0]
+            data = self.data[different_idx]
+            img2 = self.image_reshape(data)
+            same_label = 0
 
-        return image, label
+        if self.trans1:
+            img = self.trans1(img)
+            img2 = self.trans1(img2)
+
+        return img, img2, same_label
+
+
+def set_trans():
+    trans1 = transforms.Compose([
+        transforms.Resize((32, 32)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+    return trans1
+
+
+if __name__ == '__main__':
+    data_path = '/Users/yuki_kumon/Documents/python/Siamese-Network-Practice/data/cifar-10-batches-py'
+    cifar_dict = unpickle(data_path + '/data_batch_1')
+
+    trans1 = set_trans()
+    dataset = MyDataset(cifar_dict, trans1)
+    print(dataset[100])
