903ae9a8ba72862eda79e979aeb726957b52709f
Yuki Kumon <yuki-kumon@g.ecc.u-tokyo.ac.jp>
2019-08-04 19:00:08 +0900

    dataloader書きました。次はsanity check

--- misc/Dataloader_cifar_10.py
+++ misc/Dataloader_cifar_10.py
@@ -14,10 +14,12 @@ from torchvision import transforms
 # import torch.nn as nn  # ネットワーク構築用
 # import torch.optim as optim  # 最適化関数
 # import torch.nn.functional as F  # ネットワーク用の様々な関数
-# import torch.utils.data  # データセット読み込み関連
+import torch.utils.data  # データセット読み込み関連
 
 import numpy as np
 from PIL import Image
+
+import os
 
 
 def unpickle(file):
@@ -89,10 +91,40 @@ def set_trans():
     return trans1
 
 
+def Dataloader_cifar_10(cifar_path, batch_size=16, shuffle=True):
+    '''
+    DataLoader for cifar 10
+    リストで返す
+    '''
+
+    trans1 = set_trans()
+    train_loader = []
+
+    # sat path
+    train_path = []
+    for i in range(1, 6):
+        train_path.append(os.path.join(cifar_path, 'data_batch_' + str(i)))
+    test_path = os.path.join(cifar_path, 'test_batch')
+
+    # set dataloader
+    for i in train_path:
+        train_loader.append(torch.utils.data.DataLoader(MyDataset(unpickle(i), trans1), batch_size=16, shuffle=True))
+    test_loader = torch.utils.data.DataLoader(MyDataset(unpickle(test_path), trans1), batch_size=16, shuffle=True)
+
+    return train_loader, test_loader
+
+
 if __name__ == '__main__':
+    """
+    sanity check
+    """
     data_path = '/Users/yuki_kumon/Documents/python/Siamese-Network-Practice/data/cifar-10-batches-py'
+
+    # sataset sanity check
     cifar_dict = unpickle(data_path + '/data_batch_1')
 
     trans1 = set_trans()
     dataset = MyDataset(cifar_dict, trans1)
-    print(dataset[100])
+    dataset[100]
+
+    # DataLoader sanity check
