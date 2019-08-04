85ded27b824e5ae2381f938c8928e0a3e34ac340
Yuki Kumon <yuki-kumon@g.ecc.u-tokyo.ac.jp>
2019-08-04 19:32:04 +0900

    ロス関数を追加。

---
+++ models/ContrastiveLoss.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+"""
+Loss function
+copied from https://vaaaaaanquish.hatenablog.com/entry/2019/02/23/214036
+Author :
+    Yuki Kumon
+Last Update :
+    2019-08-04
+"""
+
+
+import torch
+
+
+class ContrastiveLoss(torch.nn.Module):
+    def __init__(self, margin=1.0):
+        super(ContrastiveLoss, self).__init__()
+        self.margin = margin
+
+    def forward(self, x0, x1, y):
+        diff = x0 - x1
+        dist_sq = torch.sum(torch.pow(diff, 2), 1)
+        dist = torch.sqrt(dist_sq)
+        mdist = self.margin - dist
+        dist = torch.clamp(mdist, min=0.0)
+        loss = y * dist_sq + (1 - y) * torch.pow(dist, 2)
+        loss = torch.sum(loss) / 2.0 / x0.size()[0]
+        return loss
--- train.py
+++ train.py
@@ -16,16 +16,41 @@ import torch.optim as optim  # 最適化関数
 import torch.nn.functional as F  # ネットワーク用の様々な関数
 import torch.utils.data  # データセット読み込み関連
 
+from models.Siamese_Network import Siamese_Network
+from dataloader.Dataloader_cifar_10 import Dataloader_cifar_10
+
 import numpy as np
 from PIL import Image
 
-# from torchsummary import summary
 from tensorboardX import SummaryWriter
 
 # import matplotlib.pyplot as plt
 
-from misc.attr_dict import AttributeDict
 from absl import app, flags, logging
+from absl.flags import FLAGS
 
 
 flags.DEFINE_string('cifar_path', './data/cifar-10-batches-py', 'cifar 10 dataset path')
+flags.DEFINE_float('same_rate', 0.5, 'positive rate when training')
+flags.DEFINE_integer('depth', 5, 'CNN depth')
+flags.DEFINE_float('dropout_ratio', 0.1, 'CNN dropout ratio')
+flags.DEFINE_integer('epoch', 20, 'epoch number')
+flags.DEFINE_bool('pre_trained', False, 'whether model is pretrained or not')
+flags.DEFINE_string('model_path', './model.tar', 'model path')
+
+
+def main(_argv):
+    # set model
+    Net = Siamese_Network(input_channels=3, depth=FLAGS.depth, dropout_ratio=FLAGS.dropout_ratio)
+
+    # set dataloader
+    train_loader, test_loader = Dataloader_cifar_10(FLAGS.cifar_path)
+
+    # define training function
+
+
+if __name__ == '__main__':
+    try:
+        app.run(main)
+    except SystemExit:
+        pass
